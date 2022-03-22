12447e0c8d91dbb4105a309eb6cddc6f0c117ffb
DT271199 <79830614+DT271199@users.noreply.github.com>
2022-03-22 13:11:08 +0700

    commit lúc 1h10 22/3

--- 1811061689-NguyenHoangDuyThai-BigSchool/ViewModels/CourseViewModel.cs
+++ 1811061689-NguyenHoangDuyThai-BigSchool/ViewModels/CourseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using _1811061689_NguyenHoangDuyThai_BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +10,43 @@ namespace _1811061689_NguyenHoangDuyThai_BigSchool.ViewModels
     public class CourseViewModel
     {
         public string Place { get; set; }
+
         public string Date { get; set; }
+
         public string Time { get; set; }
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }

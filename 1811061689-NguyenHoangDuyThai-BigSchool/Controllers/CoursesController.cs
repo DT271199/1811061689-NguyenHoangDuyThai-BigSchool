2a3fcf039f4a56b756300798f889778906302def
DT271199 <79830614+DT271199@users.noreply.github.com>
2022-03-23 20:13:19 +0700

    commit lúc 8h12 23/3

--- 1811061689-NguyenHoangDuyThai-BigSchool/Controllers/CoursesController.cs
+++ 1811061689-NguyenHoangDuyThai-BigSchool/Controllers/CoursesController.cs
@@ -29,6 +29,7 @@ namespace _1811061689_NguyenHoangDuyThai_BigSchool.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
             if (!ModelState.IsValid)

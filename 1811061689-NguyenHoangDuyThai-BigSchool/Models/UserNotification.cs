358fe973a87c6bcc25d666ab252cd15e55c0ba74
DT271199 <79830614+DT271199@users.noreply.github.com>
2022-04-02 15:00:13 +0700

    commit lúc 3h 4/2

--- 1811061689-NguyenHoangDuyThai-BigSchool/Models/UserNotification.cs
+++ 1811061689-NguyenHoangDuyThai-BigSchool/Models/UserNotification.cs
@@ -33,4 +33,20 @@ namespace _1811061689_NguyenHoangDuyThai_BigSchool.Models
     public class Notification
     {
     }
+
+    public class UserNotification
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int NotificationId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Notification Notification { get; set; }
+
+        public bool IsRead { get; set; }
+    }
 }
